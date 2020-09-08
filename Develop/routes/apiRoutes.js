51367d8fdc4ac60204d1730eb01ce352c50ada17
Carter Keith <carterkeith3gmail.com>
2020-09-08 19:38:08 -0400

    fourth commit

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,21 +1,16 @@
-// Connect routes to journal data.
 var journalData = require("../data/journalData");
 
-// Routing
 module.exports = function (app) {
 
-  // GET request
   app.get("/api/journals", function (req, res) {
     res.json(journalData);
   });
 
-  // POST request
   app.post("/api/journals", function (req, res) {
     journalData.push(req.body);
     res.json("saved");
   });
 
-  // DELETE request
   app.delete("/api/journals/:index", function (req, res) {
     var elem = parseInt(req.params.index);
     var tempjournal = [];
